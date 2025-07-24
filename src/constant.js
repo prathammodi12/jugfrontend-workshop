f86363cf4a51dd2c00c7c9106d30b47ff63ee8bb
Vaibhav Savaliya <154512212+vaibhav-s-7span@users.noreply.github.com>
2025-07-24 18:32:56 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,4 @@
-export const endpoint = 'http://15.206.164.156:8080';
+// export const endpoint = 'http://15.206.164.156:8080';
+export const endpoint = 'https://743gyaarbc.execute-api.ap-south-1.amazonaws.com';
 
 export const API_PATH = '/api/users';
